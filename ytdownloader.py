bc1c7642359fd3edd0c04c6588d26029c1608271
hossam-khalaf <hossamhassan303089@gmail.com>
2021-01-04 02:48:02 +0200

    filtering && change YT link

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,13 +1,28 @@
 from pytube import YouTube
 
-link = 'https://www.youtube.com/watch?v=3HqN-YS8eso'
+# youtube link from freecodecamp channel >> Discord bot
+link = 'https://www.youtube.com/watch?v=SPTfmiYiuok'
 
 #link = input('please enter youtube URL: )
 
 video = YouTube(link)
 
-print(f" the video title is:\n{video.title} \n--------------")
-print(f" the video description is:\n{video.description}\n--------------")
-print(f" the video views is:\n{video.views} \n--------------")
-print(f" the video rating is:\n{video.rating} \n--------------")
-print(f" the video duration is:\n{video.length/60} minutes \n--------------")
+# print(f" the video title is:\n{video.title} \n--------------")
+# print(f" the video description is:\n{video.description}\n--------------")
+# print(f" the video views is:\n{video.views} \n--------------")
+# print(f" the video rating is:\n{video.rating} \n--------------")
+# print(f" the video duration is:\n{video.length/60} minutes \n--------------")
+
+#looping through strem to print the array nicely
+# for stream in video.streams:
+#   print(stream)
+
+
+#filtering results
+# for stream in video.streams.filter(progressive=True):
+#   print(stream)
+
+for stream in video.streams.filter(res='720p'):
+  print(stream)
+
+
